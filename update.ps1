652afe884e2255a32d6d76be555ff0181e52b4f4
Hans <87748222+Hanselkek@users.noreply.github.com>
2023-08-31 14:47:00 +0800

    Add files via upload

--- update.ps1
+++ update.ps1
@@ -3,8 +3,8 @@ if (Test-Path "$env:USERPROFILE\AppData\Local\spicetify") {
     Write-Host "Spicetify enviroment found.."
 
     # Update and apply spicetify
-    spicetify update
-    spicetify apply
+    spicetify upgrade
+    spicetify restore backup apply
     Write-Host "Spicetify has been updated and applied."
 } else {
     Write-Host "Spicetify enviroment not found."
